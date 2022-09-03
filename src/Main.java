e8cbb1ca40e76db7002cb0066545e3eec188b45f
Choijiwoong <gogogi313@naver.com>
2022-09-03 17:39:21 +0900

    [DOCS]주석을 달때는 DOCS

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-//막
+//막 좀더 뭐라고
 class Japengi{
     HashMap<String, Integer> priceMap=new HashMap<>();
     HashMap<String, Integer> countMap=new HashMap<>();
